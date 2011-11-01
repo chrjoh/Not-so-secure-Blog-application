b277da2e4c18ba3125a03d58a0529df3d3e8fb57
chrjoh <christerjoha@gmail.com>
2011-11-01 19:09:09 +0100

    initila setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nssba::Application.config.secret_token = '81040fd14e72288e8642ee2f0880d4d7112c762b93ffd80560a8bb654b27a75663e18c9fe4d4a27b97c226ee555cc0d8a288d9a8b3c36529a46ba02dfed0160e'
