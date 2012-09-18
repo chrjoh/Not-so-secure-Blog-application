d599d5a789461c017aaae0100a6c6c8ff4906694
chrjoh <christerjoha@gmail.com>
2012-09-18 18:01:01 +0200

    addec ci script

---
+++ ci.sh
@@ -0,0 +1,5 @@
+#!/bin/bash --login
+source .rvmrc
+bundle install --path .bundle --without development stage production
+bundle exec rake db:migrate
+COVERAGE=on LC_CTYPE=en_US.UTF-8 bundle exec rake 
